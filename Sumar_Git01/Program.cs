fb49cfb722643a9fb404bb09632a7816169cb41c
JesusDiez <jesdiez@ucm.es>
2026-02-07 11:18:17 +0100

    Agregar archivos de proyecto.

---
+++ Sumar_Git01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sumar_Git01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
