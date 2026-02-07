a598199adc860738f01de4123f8adc89fd269e09
JesusDiez <jesdiez@ucm.es>
2026-02-07 16:10:45 +0100

    =Restarura el fichero Program.cs a una version del menú sin el bucle

--- Sumar_Git01/Program.cs
+++ Sumar_Git01/Program.cs
@@ -6,49 +6,37 @@
         {
             //variables
             double num1, num2, resultado;
-            int option;
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("Elige una opcion:");
+            int option = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriber el primer numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
 
-            do {
-               
-                Console.WriteLine("1. Sumar");
-                Console.WriteLine("2. Restar");
-                Console.WriteLine("3. Multiplicar");
-                Console.WriteLine("4. Salir");
-                Console.WriteLine("Elige una opcion:");
-                option = int.Parse(Console.ReadLine());
-                Console.WriteLine("Escriber el primer numero: ");
-                num1 = int.Parse(Console.ReadLine());
-                Console.WriteLine("Escribe el segundo número: ");
-                num2 = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    resultado = Suma(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                 case 2:
+                    resultado = Resta(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                case 3:
+                     resultado = Multiplica(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                default: Console.WriteLine("Valores no validos");
+                break; 
+            }
 
-
-                switch (option)
-                {
-                    case 1:
-                        resultado = Suma(num1, num2);
-                        Console.WriteLine($"El resultado de la suma es: {resultado}");
-                        break;
-                    case 2:
-                        resultado = Resta(num1, num2);
-                        Console.WriteLine($"El resultado de la suma es: {resultado}");
-                        break;
-                    case 3:
-                        resultado = Multiplica(num1, num2);
-                        Console.WriteLine($"El resultado de la suma es: {resultado}");
-                        break;
-                    case 4:
-                        Console.WriteLine("Saliendo....");
-                        break;
-                    default:
-                        Console.WriteLine("Valores no validos");
-                        break;
-                }
-       
-            } while (option != 1 && option !=   2 && option != 3 && option != 5);
         }
         //metodos
-
-        static double Suma(double num1, double num2) 
+       static double Suma(double num1, double num2) 
         {
             double resultado= num1 + num2;   
             return resultado;
