d9b3107b2d2bbbc5101796493bb92c39af32884c
JesusDiez <jesdiez@ucm.es>
2026-02-07 13:31:23 +0100

    crear el menu

--- Sumar_Git01/Program.cs
+++ Sumar_Git01/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //variables
+            double num1, num2, resultado;
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("Elige una opcion:");
+            int option = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escriber el primer numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    resultado = Suma(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                 case 2:
+                    resultado = Resta(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                case 3:
+                     resultado = Multiplica(num1, num2);
+                    Console.WriteLine($"El resultado de la suma es: {resultado}");
+                    break;
+                default: Console.WriteLine("Valores no validos");
+                break; 
+            }
+
         }
 
        static double Suma(double num1, double num2) 
@@ -12,12 +41,12 @@
             double resultado= num1 + num2;   
             return resultado;
         }
-            static double Resta(double num1, double num2) 
+       static double Resta(double num1, double num2) 
         {
             double resultado= num1 - num2;   
             return resultado;
         }
-          static double Mutiplica(double num1, double num2) 
+      static double Multiplica(double num1, double num2) 
         {
             double resultado= num1 * num2;   
             return resultado;
